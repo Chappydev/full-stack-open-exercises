d73a0ef10b7defd41a329d7195b3bcb758717495
Chappydev <danjtchapman@gmail.com>
2022-08-21 15:42:16 -0600

    Complete 1.8

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -12,13 +12,27 @@ const Value = ({ type, value }) => (
   </p>
 )
 
+const Statistics = ({ good, neutral, bad }) => {
+  const average = ( good - bad ) / ( good + neutral + bad );
+  const positiveRate = ( good / ( good + neutral + bad ) ) * 100;
+
+  return (
+    <>
+      <h2>statistics</h2>
+      <Value type="good" value={good} />
+      <Value type="neutral" value={neutral} />
+      <Value type="bad" value={bad} />
+      <Value type="average" value={average} />
+      <Value type="positive" value={positiveRate + "%"} />
+    </>
+  )
+}
+
 const App = () => {
   // save clicks of each button to its own state
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
-  const average = ( good - bad ) / ( good + neutral + bad );
-  const positiveRate = ( good / ( good + neutral + bad ) ) * 100;
 
   const increment = (value, setValue) => () => {
     setValue(value + 1);
@@ -30,12 +44,7 @@ const App = () => {
       <Button onClick={increment(good, setGood)} text="good" />
       <Button onClick={increment(neutral, setNeutral)} text="neutral" />
       <Button onClick={increment(bad, setBad)} text="bad" />
-      <h2>statistics</h2>
-      <Value type="good" value={good} />
-      <Value type="neutral" value={neutral} />
-      <Value type="bad" value={bad} />
-      <Value type="average" value={average} />
-      <Value type="positive" value={positiveRate + "%"} />
+      <Statistics good={good} neutral={neutral} bad={bad} />
     </div>
   )
 }
